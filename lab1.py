cb38f3e34c42a9687fd37ae629235a2a6597464f
AnTranHoai <149464690+AnTranHoai38@users.noreply.github.com>
2024-06-22 12:14:55 +0700

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,6 @@
+#//////////////////////////////////////////////
+cls = str(input("Enter the collage class:"))
+adj = str(input("Enter the adjective:"))
+act = str(input("Enter an activity:"))
+print(f"{cls} class were very really {adj}. We learned how to {act} today in class"
+      f"I can\'t wait for tommorow\'s class!")
---
+++ lab2.py
@@ -0,0 +1,18 @@
+lst = []
+num = 10
+sum = 0
+for i in range(num):
+    i = float(input("Enter the score:"))
+    lst.append(i)
+    sum = sum + i
+lst.sort()
+print(f"Highest score: {lst[num-1]}\nLowest score: {lst[0]}")
+print("Average score:", sum / num)
+print("The second largest score:", lst[num-2])
+for i in lst:
+    if (i > 100):
+        print("A value over 100 has been entered!")
+print("Average score after droping two lowest score:", (sum - lst[0]-lst[1])/(num -2))
+
+
+
